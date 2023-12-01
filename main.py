d9527d88791cac1aeb13f3f98f4c6138a1ca629b
baburtemur <zavalovila321@gmail.com>
2023-12-01 18:11:41 +0300

    xDDDDDDDD

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 def main():
-    print('My first git program')
+    from PyQt6.QtGui import QPainter, QBrush, QPen
+    from PyQt6.QtWidgets import QMainWindow
+    from PyQt6 import uic
+    class Problem(QMainWindow):
+        def __init__(self):
+            super.__init__()
+            uic.loadUi('UI.ui')
+
 
 
 if __name__ == '__main__':
     main()
-    print('xxx')
